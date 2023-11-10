75ecae44e48e3ebd22ebd23ae6669528ef2bcaba
Poonam229 <poonam.chauhan229@gmail.com>
2023-11-10 11:03:41 +0530

    b51wd project

--- Model/movieModel.js
+++ Model/movieModel.js
@@ -1,14 +1,13 @@
-const mongoose = require("mongoose");
+const mongoose=require('mongoose')
+// movie SChema
 
-// Define the movie schema
-const movieSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  poster: { type: String, required: true },
-  rating: { type: Number, required: true },
-  summary: { type: String, required: true },
-  trailer: { type: String, required: true },
-});
+const movieSchema=new mongoose.Schema({
+    name:{type:String,required:true},
+    poster:{type:String,required:true},
+    rating:{type:Number,required:true},
+    summary:{type:String,required:true},
+    trailer:{type:String,required:true},
+})
 
-// Create the movie model based on the schema
-const Movie = mongoose.model("Movie", movieSchema);
-module.exports=Movie
+const Movie=mongoose.model('Movie',movieSchema)
+module.exports=Movie;
--- Route/movieRoute.js
+++ Route/movieRoute.js
@@ -2,66 +2,51 @@ const express=require('express')
 const router=express.Router()
 const Movie=require('../Model/movieModel')
 
-// Route to create a new movie
-router.post('/movies', async (req, res) => {
-    const newMovie=new Movie(req.body)
-  
-    try { 
-    await newMovie.save();
-      res.send(newMovie)
-    } catch (error) {
-      res.send({ message: error.message });
-    }
-  });
+//Added movie to DB
+router.post('/movies',async(req,res)=>{  
+        console.log(req.user)  
+       const newMovie=new Movie(req.body)
+        try{
+            await newMovie.save()
+            res.send(newMovie)
+        }catch(e){res.send({message:e})}
+    })
 
-// Route to get all movies
-router.get('/movies', async (req, res) => {
-  try {
-    const movies = await Movie.find();
-    res.send(movies);
-  } catch (error) {
-    res.send({ message: error.message });
-  }
-});
+// Get All Movies of all the users
+router.get('/movies',async(req,res)=>{
+      try{
+        const getAllMovies=await Movie.find({})//ModelName.MethodName()
+        res.send(getAllMovies)
+    }catch(e){res.send({message:e})}
+})
 
-// Route to find a movie by ID
-router.get('/movies/:id', async (req, res) => {
-    try {
-      const findmovie = await Movie.findById(req.params.id)
-      if (!findmovie) {
-        return res.status(404).send({ message: 'Movie not found' });
-      }
-      res.send(findmovie);
-    } catch (error) {
-      res.send({ message: error.message });
-    }
-  });
+router.get('/movies/:id',async(req,res)=>{
+    console.log(req.params.id)
+    try{
+      const getAllMovies=await Movie.findById(req.params.id)//ModelName.MethodName()
+      res.send(getAllMovies)
+  }catch(e){res.send({message:e})}
+})
 
-// Route to update a movie by ID
-router.patch('/movies/:id', async (req, res) => {
-  try {
-    const updatedMovie = await Movie.findByIdAndUpdate(req.params.id, req.body,{ new: true,runValidators:true})
-    if (!updatedMovie) {
-      return res.status(404).send({ message: 'Movie not found' });
-    }
-    res.send(updatedMovie);
-  } catch (error) {
-    res.send({ message: error.message });
-  }
-});
+router.patch('/movies/:id',async(req,res)=>{
+    // FROM DB
+    // Find
+    //res.send(req.params.id)
+    try{
+        const updateMovie=await Movie.findByIdAndUpdate(req.params.id,req.body,{new:true,runValidators:true})//ModelName.MethodName()       
+        res.send(updateMovie)
+    }catch(e){res.send({message:e})}
+})
 
-// Route to delete a movie by ID
-router.delete('/movies/:id', async (req, res) => {
-  try {
-    const deletedMovie = await Movie.findByIdAndDelete(req.params._id);
-    if (!deletedMovie) {
-      return res.status(404).send({ message: 'Movie not found' });
-    }
-    res.send({ message: 'Movie deleted successfully' });
-  } catch (error) {
-    res.status(400).send({ message: error.message });
-  }
-});
 
-module.exports = router;
+router.delete('/movies/:id',async(req,res)=>{
+    // FROM DB
+    // Find
+    //res.send(req.params.id)
+    try{
+        const DeleteMovie=await Movie.findByIdAndDelete(req.params.id)//ModelName.MethodName()       
+        res.send(DeleteMovie)
+    }catch(e){res.send({message:e})}
+})
 
+module.exports=router
